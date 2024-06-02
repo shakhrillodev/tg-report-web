bd426569069da86ff10a2d894438552c5d70a70c
shaxrillo <shaxrillonusratov@gmail.com>
2024-06-02 14:20:22 +0500

    modified

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
+import CryptoJS from "crypto-js";
 window.addEventListener("load", ()=>{
     const telegram = window.Telegram.WebApp;
     telegram.ready()
-    console.log(telegram.initData);
-    console.log(telegram.initDataUnsafe);
-    console.log(telegram);
+    let data = telegram.initData;
+    
+    async function validateInitData(
+        telegramInitData, BOT_TOKEN)
+    {
+      const initData = new URLSearchParams(telegramInitData);
+      let dataToCheck;
+    
+      initData.sort();
+      initData.forEach((val, key) => key !== 'hash' && dataToCheck.push(`${key}=${val}`));
+    
+      const secret = CryptoJS.HmacSHA256(BOT_TOKEN, 'WebAppData');
+      const _hash = CryptoJS.HmacSHA256(dataToCheck.join("\n"), secret).toString(CryptoJS.enc.Hex);
+    
+      return _hash === initData.get('hash');
+    }
+    
+    let token = "6557250135:AAHEZMI3Z0m3sEJ05ur4h5PpolRUvYIz01g"
+    
+    console.log(validateInitData(data, token));
 })
+
