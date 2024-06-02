c952c581c03f8c35b447d1c9c74e84bfa8f38560
shaxrillo <shaxrillonusratov@gmail.com>
2024-06-02 11:07:03 +0500

    nothing

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ window.addEventListener("load", ()=>{
     const telegram = window.Telegram.WebApp;
     telegram.ready()
     console.log(telegram.initData);
+    console.log(telegram.initDataUnsafe);
+    console.log(telegram);
 })
