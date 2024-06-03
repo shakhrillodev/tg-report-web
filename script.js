53c0cd7d8bf244803724057437f54fd6208a3c56
shaxrillo <shaxrillonusratov@gmail.com>
2024-06-03 13:23:05 +0500

    dev

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ window.addEventListener("load", async ()=>{
     let body = document.querySelector('body')
     body.innerHTML = data
     await telegram.sendData(data)
+    .then(ctx=>{
+      console.log("Send succesfully", ctx);
+    }).catch(e=>{
+      console.log(e);
+    })
     
     // async function getData(){
     //   const res = await fetch("http://127.0.0.1:3000", {
