35cf41e2eaa70160b9fee8d116bb8253416c1d10
shaxrillo <shaxrillonusratov@gmail.com>
2024-06-03 13:11:22 +0500

    dev

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.addEventListener("load", ()=>{
     telegram.ready()
     let data = telegram.initData;
     let body = document.querySelector('body')
-    body.appendChild(data)
+    body.innerHTML = data
     // async function getData(){
     //   const res = await fetch("http://127.0.0.1:3000", {
     //     headers: {
