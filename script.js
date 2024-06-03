7c77df09b5ccfff58a688712e19f4b34c93d77c9
shaxrillo <shaxrillonusratov@gmail.com>
2024-06-03 12:37:09 +0500

    dev

--- script.js
+++ script.js
@@ -2,24 +2,12 @@ window.addEventListener("load", ()=>{
     const telegram = window.Telegram.WebApp;
     telegram.ready()
     let data = telegram.initData;
-    
-    async function validateInitData(
-        telegramInitData, BOT_TOKEN)
-    {
-      const initData = new URLSearchParams(telegramInitData);
-      let dataToCheck = [];
-    
-      initData.sort();
-      initData.forEach((val, key) => key !== 'hash' && dataToCheck.push(`${key}=${val}`));
-    
-      const secret = CryptoJS.HmacSHA256(BOT_TOKEN, 'WebAppData');
-      const _hash = CryptoJS.HmacSHA256(dataToCheck.join("\n"), secret).toString(CryptoJS.enc.Hex);
-    
-      return _hash === initData.get('hash');
+    async function getData(){
+      const res = await fetch("http://127.0.0.1:3000", {
+        headers: {
+          Authorization: `tma ${initDataRaw}`
+        }
+      })
+      console.log(res);
     }
-    
-    let token = "6557250135:AAHEZMI3Z0m3sEJ05ur4h5PpolRUvYIz01g"
-    
-    console.log(validateInitData(data, token));
 })
-
